357be5eb1269cb5dc98d863134e6f77ce5d437b9
mrOrlando <juriy88@mail.com>
2018-01-26 15:48:46 +0300

    Change project structure. Add gulp tasks.

---
+++ TODO.md
@@ -0,0 +1,4 @@
+## TODO
+
+* [ ] Use font-awesome via node_modules.
+* [ ] Add fonts from googlefonts into project.
--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,63 @@
 var gulp = require('gulp');
-var stylus = require('gulp-stylus');
+var browserSync = require('browser-sync').create();
+var mainNpmFiles = require('gulp-main-npm-files');
 var sourcemaps = require('gulp-sourcemaps');
+var stylus = require('gulp-stylus');
+var pug = require('gulp-pug');
 
 gulp.task('styl', function() {
   gulp
-    .src('./src/stylus/*.styl')
+    .src('./src/stylus/app.styl')
     .pipe(sourcemaps.init())
-    .pipe(stylus())
+    .pipe(stylus({ include: 'node_modules' }))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./src/css'));
+    .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('pug', function() {
+  return gulp
+    .src('./src/pug/pages/*.pug')
+    .pipe(
+      pug({
+        pretty: true,
+      }),
+    )
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('copy-assets', function() {
+  return gulp.src('./src/assets/**').pipe(gulp.dest('build'));
+});
+
+gulp.task('copy-npm-files', function() {
+  gulp.src(mainNpmFiles()).pipe(gulp.dest('build/vendor'));
+  gulp
+    .src('./node_modules/normalizecss/normalize.css')
+    .pipe(gulp.dest('build/vendor'));
+  gulp
+    .src('./node_modules/font-awesome/css/font-awesome.min.css')
+    .pipe(gulp.dest('build/vendor'));
+});
+
+gulp.task('build', function() {
+  gulp.run(['styl', 'pug', 'copy-assets', 'copy-npm-files']);
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./src/stylus/*.styl', ['styl']);
+  gulp.watch('./package.json', ['copy-npm-files']);
+  gulp.watch('./src/stylus/**/*.styl', ['styl']);
+  gulp.watch('./src/pug/**/*.pug', ['pug']);
+  gulp.watch('./src/assets/**/*.*', ['copy-assets']);
 });
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: './build',
+    },
+  });
+  gulp.run('watch');
+  browserSync.watch('./build', browserSync.reload);
+});
+
+gulp.task('default', ['build', 'serve']);
